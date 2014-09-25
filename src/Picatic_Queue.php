dd024dd50b763f24730e4f37321083039e21c373
James Sapara <james.sapara@gmail.com>
2014-09-25 13:15:27 -0700

    - removed defaulting to empty array on createJob

--- src/Picatic_Queue.php
+++ src/Picatic_Queue.php
@@ -2,7 +2,7 @@
 
 class Picatic_Queue extends Picatic_Model {
 
-  public function createJob($job=array()) {
+  public function createJob($job) {
     return $this->classActionWithParams('jobs', null, 'post', $job);
   }
 
